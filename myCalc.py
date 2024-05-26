7c418f6eaf170a1b99f64ed80a5087fcb6f17796
Mohammad Imran Khan <imukhan251995@gmail.com>
2024-05-26 12:16:25 +0530

    Add files via upload
    It performs 4 basic arithmatic operstions

---
+++ myCalc.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    return a+b
+
+def mul(a,b):
+    return a*b
+
+def sub(a, b):
+    return a-b
+
+def div(a,b):
+    if b == 0 :
+        return 'Division not Possible'
+    return a/b
+
+pi = 3.14
+e = 2.7
